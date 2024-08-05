d612b79ede7803381f5dd2e257d5068aae95ba45
xfzhang0602 <1505528051@qq.com>
2024-08-05 21:58:47 +0800

    20240805

---
+++ 20240804_1_VS Debugging/20240804_1_VS Debugging/test.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int num = 100;
+	char c = 'w';
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+	}
+	return 0;
+}
+
